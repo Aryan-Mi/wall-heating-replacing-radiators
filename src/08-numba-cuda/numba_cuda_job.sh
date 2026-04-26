1ef483f5fc6382d4c6b7f9e43e2f0d3e9d71aa72
rifo <horbatenko.vladislav@gmail.com>
2026-04-27 00:56:47 +0200

    Tasks 8 and 9

---
+++ src/08-numba-cuda/numba_cuda_job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#BSUB -J wallheat_numba_cuda
+#BSUB -q gpua100
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -n 1
+#BSUB -R "rusage[mem=4GB]"
+#BSUB -W 00:20
+#BSUB -oo outputs/08-numba-cuda/%J.out
+#BSUB -eo outputs/08-numba-cuda/%J.err
+
+set -eo pipefail
+cd "$LS_SUBCWD"
+mkdir -p outputs/08-numba-cuda
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+python src/08-numba-cuda/run_task_8.py \
+  --subset 10 \
+  --data-dir /dtu/projects/02613_2025/data/modified_swiss_dwellings/ \
+  --output-dir outputs/08-numba-cuda \
+  --reference-csv outputs/02-reference-benchmark-and-visualization/timing_summary.csv
---
+++ src/09-cupy/cupy_job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#BSUB -J wallheat_cupy
+#BSUB -q gpua100
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -n 1
+#BSUB -R "rusage[mem=4GB]"
+#BSUB -W 00:20
+#BSUB -oo outputs/09-cupy/%J.out
+#BSUB -eo outputs/09-cupy/%J.err
+
+set -eo pipefail
+cd "$LS_SUBCWD"
+mkdir -p outputs/09-cupy
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+python src/09-cupy/run_task_9.py \
+  --subset 10 \
+  --data-dir /dtu/projects/02613_2025/data/modified_swiss_dwellings/ \
+  --output-dir outputs/09-cupy \
+  --reference-csv outputs/02-reference-benchmark-and-visualization/timing_summary.csv
